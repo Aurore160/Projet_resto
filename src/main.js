4fe30ccf0e40dc5c88735ca2ceae8184d4aac756
Aurore160 <aurore.n-sasi@2029.ulc-icam.com>
2025-10-27 11:41:31 +0100

    nouveaux fichier de la page d'acceuille a modifier

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 import App from './App.vue'
+// Supprimer la ligne: import router from './router'
 
-createApp(App).mount('#app')
+createApp(App).mount('#app')  // Supprimer .use(router)
